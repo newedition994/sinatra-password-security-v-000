e4c897aaf132448da3d15a9e49cf672ee19ebfe3
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-04 02:41:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,10 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		#your code here!
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+    end
 	end
 
 	get "/success" do
